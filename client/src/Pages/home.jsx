e962122e18aa9a43ec53d45c7aecf3cd3297661e
borebrian2021 <bkimutai2021@gmail.com>
2023-05-05 20:16:37 +0300

    Added Daisy UI avatars

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -6,10 +6,32 @@ class Home extends Component {
             <>
                 <div className="hero min-h-screen bg-base-200">
                     <div className="hero-content flex-col lg:flex-row">
-                        <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                        <img src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1683296721/mern-authentication/pexels-tain%C3%A1-bernard-3586091_o1ub2a.jpg" className="max-w-sm rounded-lg shadow-2xl h-[200px]" />
                         <div>
-                            <h1 className="text-5xl font-bold">Welcome To </h1>
-                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                            <h3 className="text-5xl font-bold">Welcome To The home of champions</h3>
+                            <div className="avatar-group -space-x-6">
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    </div>
+                                </div>
+                                <div className="avatar placeholder">
+                                    <div className="w-12 bg-neutral-focus text-neutral-content">
+                                        <span>+99</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="py-6">Keep visiting for more cool updates</p>
                             <button className="btn btn-primary">Get Started</button>
                         </div>
                     </div>
