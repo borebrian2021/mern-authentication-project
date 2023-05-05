c222dac0eedfb2f52191e0bea41dcce6f1769ced
borebrian2021 <bkimutai2021@gmail.com>
2023-05-05 07:11:50 +0300

    Initialized node backend server.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: [],
   theme: {
     extend: {},
-    fontFamily: {
-      'jost': ['Jost','sans-serif'],
-      'dancing': ['Dancing Script', 'cursive'],
-      'cormorant': ['Cormorant Garamond','serif'],
-    },
   },
   plugins: [],
 }
+
