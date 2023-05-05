d0290f44e2f9513b230596a431a6af3cff658fb3
borebrian2021 <bkimutai2021@gmail.com>
2023-05-05 07:26:49 +0300

    Configured Tailwind css successfully.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      'jost': ['Jost','sans-serif'],
+      'dancing': ['Dancing Script', 'cursive'],
+      'cormorant': ['Cormorant Garamond','serif'],
+    },
   },
   plugins: [],
 }
-
