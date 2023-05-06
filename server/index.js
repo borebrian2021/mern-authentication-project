438fbaa898067fade2ad2f0243a3fce016c3acf9
borebrian2021 <bkimutai2021@gmail.com>
2023-05-06 11:49:07 +0300

    Successfully consumed register new user API

--- server/index.js
+++ server/index.js
@@ -1,17 +1,39 @@
 const express = require('express')
 const app =express()
 const cors =require('cors')
-
-
+const mangoose =require('mongoose')
+const User = require('./models/user.models')
 
 //CONFIGURE CORS
 app.use(cors())
+app.use(express.json())
+
+
+
+//CONNECT TO DB
+mangoose.connect('mongodb://localhost:27017/mern-auth')
 
 
 //REGISTER USER ENDPOINT
-app.post('/api/register', (req,res)=>{
-console.log(req.body)
-res.json({status:"ok"})
+app.post('/api/register', async (req,res)=>{
+
+try{
+    await User.create({
+        fullNames: req.body.fullNames,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        gender: req.body.gender,
+        profileUrl: req.body.profileLink,
+        password: req.body.confirmPassword
+    
+    })
+res.json({status:"OK", error:"Data inserted successfully!"})
+
+}
+catch (err) {
+console.log(err)
+res.json({status:err, error:"Duplicate email address"})
+}
 
 })
 app.listen(1337,()=>{
---
+++ server/models/user.models.js
@@ -0,0 +1,19 @@
+
+const mongoose =require('mongoose');
+
+
+//DEFINE TABLE STRUCTURE
+const User=new mongoose.Schema({
+    fullNames: {type: String,required: true},
+    email: {type: String ,required: true,unique: true},
+    profileUrl:{type:String,required:true},
+    phoneNumber: {type:String,required:true},
+    gender: {type:String,required:true},
+    password:{type:String,required:true}
+},{
+    collection:'user-data'
+})
+
+const model = mongoose.model('UserData',User)
+
+module.exports =model
